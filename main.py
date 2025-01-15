20456aebb917dbb7b3cc1876e216f8e2cf047aef
Bilbusaur <billycodes3373@gmail.com>
2025-01-15 22:31:21 +0000

    Added notes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 def main():
     try:
-      
+     #opens and reads file, counts and displays words in the documents. 
         with open("books/frankenstein.txt") as f:
             filepath = "books/frankenstein.txt"
             file_contents = f.read()
@@ -15,7 +15,7 @@ def main():
     except FileNotFoundError:
         print("This file could not be found. Please check the path and try again. ")
 
-
+#Counts the letters only if alphabetic. 
 def count_characters(character_counts):
     characters_dict = {}
     for letter in character_counts.lower():
@@ -31,17 +31,17 @@ def count_characters(character_counts):
 def display_letters(letter_counts):
         #sort the dictionary by keys.
         sorted_letters = sorted(letter_counts.items())
-        for letter, count in sorted_letters:
+        for letter, count in sorted_letters:# Grabs key,value from the dictionary.
             print(f"The character '{letter}' appears {count} times.")
     
 
-
+#Executes only on call, helps with imports I think.
 if __name__ == "__main__":
-
-    contents = main()
-    if contents:
+    #calls open and read function.
+    contents = main() #defines the document to work with.
+    if contents:#Only runs if there is content.
         character_counts = count_characters(contents)
-        diction = count_characters(contents)
+        diction = count_characters(contents)# The dictionary returned from the count function
         display = display_letters(diction)
         
     
