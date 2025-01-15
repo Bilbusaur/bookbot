326884bc1ad58e5f164fcd9b1edf2766dd8fbdf5
Bilbusaur <billycodes3373@gmail.com>
2025-01-15 22:23:19 +0000

    Version 1 complete

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # hello-world
 My first guided project on boot.dev
 
-My name is billy.
+Bookbot: Text Analysis Tool
 
-BookBot is my first project!
+Bookbot is a simple command-line program that reads text from a file and generates a report about the text. The program is designed to help you analyze books or any other text files by counting the frequency of each character in the text. 
+
+Features
+
+Reads text from a specified file (frankenstein.txt in this case).
+Counts and reports the frequency of each letter (case-insensitive).
+Ignores non-alphabetical characters.
+Outputs the results sorted alphabetically.
--- main.py
+++ main.py
@@ -1,29 +1,56 @@
+
+
+
 def main():
     try:
-        
-        
-
-
+      
         with open("books/frankenstein.txt") as f:
-            content = []
+            filepath = "books/frankenstein.txt"
             file_contents = f.read()
-            content.append(file_contents)
-            content.split()
-            print(type(content))
-            
-
-
-            
-
-                
-            
-
-
+            print(f"--- Book Report of {filepath} ---")
+            print(f"{len(file_contents.split())} words found in the document")
+            print(" ")
             return file_contents
+
     except FileNotFoundError:
         print("This file could not be found. Please check the path and try again. ")
 
-        
-        
+
+def count_characters(character_counts):
+    characters_dict = {}
+    for letter in character_counts.lower():
+        if letter.isalpha():
+            if letter in characters_dict:
+                characters_dict[letter] += 1
+            
+            else:
+                characters_dict[letter] = 1
+    return characters_dict
+
+
+def display_letters(letter_counts):
+        #sort the dictionary by keys.
+        sorted_letters = sorted(letter_counts.items())
+        for letter, count in sorted_letters:
+            print(f"The character '{letter}' appears {count} times.")
+    
+
+
 if __name__ == "__main__":
-    main()
+
+    contents = main()
+    if contents:
+        character_counts = count_characters(contents)
+        diction = count_characters(contents)
+        display = display_letters(diction)
+        
+    
+    
+    
+    
+
+ 
+
+    
+
+
